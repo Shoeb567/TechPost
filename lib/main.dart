bce2d5665006062e56e09cff274f06e0293fd4a5
Shoeb <shoeb5823@gmail.com>
2021-04-16 15:33:56 +0530

    'TechPost'

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,18 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+//import "package:tech_post_app/TwitterHome.dart";
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:HomePage() ,
+     // home: HomePage(),
     );
   }
 }
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Tech Post"),
-        centerTitle: true,
-      ),
-      body: Container(
-
-        child: Column(
-          children: [
-            Center(
-                child: Text(
-                    "Hello World"
-                )),
-            Center(
-                child: Text(
-                    "Shoeb Shaikh"
-
-                )),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
