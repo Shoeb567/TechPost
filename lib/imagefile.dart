8cb16abe9ef8002de8840edd3ac12b37fc2b7a82
Shoeb <shoeb5823@gmail.com>
2021-04-19 11:46:23 +0530

    AppBarFeature

--- lib/imagefile.dart
+++ lib/imagefile.dart
@@ -1,9 +1,4 @@
-import "package:flutter/material.dart";
-class ImageFetch extends StatelessWidget {
+class ImageFetch{
   var logo = "appassets/TwitterLogo.png";
   var fatureicon = "appassets/Featurestrokeicon.png";
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
