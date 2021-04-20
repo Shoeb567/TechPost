be6ecebec41404ff7ac713bba2955eb6568ad3ae
Shoeb <shoeb5823@gmail.com>
2021-04-20 14:19:00 +0530

    FeatureApi

--- lib/ApiFetchScreens/show_api_data.dart
+++ lib/ApiFetchScreens/show_api_data.dart
@@ -9,7 +9,7 @@ class ShowApiData extends StatefulWidget {
 }
 
 class _ShowApiDataState extends State<ShowApiData> {
-  Future getUserData() async {
+  Future<List<User>> getUserData() async {
     final response =
         await http.get("https://jsonplaceholder.typicode.com/users");
     final jsonData = jsonDecode(response.body);
@@ -28,7 +28,7 @@ class _ShowApiDataState extends State<ShowApiData> {
       color: Colors.white,
       child: FutureBuilder(
         future: getUserData(),
-        builder: (context, snap) {
+        builder: (context, AsyncSnapshot snap) {
           if (snap.data == null) {
             return Container(
               child: Center(
@@ -36,157 +36,141 @@ class _ShowApiDataState extends State<ShowApiData> {
               ),
             );
           } else {
-            return Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: snap.data.toString().length,
-                    itemBuilder: (BuildContext context, int index) {
-                      String oneChar = snap.data[index].name
-                          .toString()
-                          .substring(0, 1)
-                          .toUpperCase();
-                      return Column(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(
-                                  color: Color.fromRGBO(183, 182, 185, 0.1),
-                                ),
+            return ListView.builder(
+              itemCount: snap.data.toString().length,
+              itemBuilder: (BuildContext context, int index) {
+                String oneChar = snap.data[index].name
+                    .toString()
+                    .substring(0, 1)
+                    .toUpperCase();
+                return Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(183, 182, 185, 0.1),
+                          ),
+                        ),
+                      ),
+                      child: ListTile(
+                        leading: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.black,
+                              child: Text(
+                                oneChar,
+                                style: TextStyle(color: Colors.white),
                               ),
                             ),
-                            child: ListTile(
-                              leading: Column(
-                                children: [
-                                  CircleAvatar(
-                                    backgroundColor: Colors.black,
-                                    child: Text(
-                                      oneChar,
-                                      style: TextStyle(color: Colors.white),
+                          ],
+                        ),
+                        title: Column(
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.favorite,
+                                  size: 12,
+                                  color: Color.fromRGBO(104, 118, 132, 1),
+                                ),
+                                Text(
+                                  '  Twitter Home',
+                                  style: TextStyle(fontSize: 10),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Text(
+                                  '${snap.data[index].name}',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Icon(Icons.verified,
+                                    size: 17, color: Colors.blue),
+                                Text(
+                                  '@',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    '${snap.data[index].username} - 10h',
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.keyboard_arrow_down_rounded,
+                                  size: 18,
+                                  color: Color.fromRGBO(189, 197, 205, 1),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                      'TechPost TechPost TechPost TechPost TechPost TechPost TechPost TechPost TechPost TechPost'),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 15),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Container(
+                                  width: 50,
+                                  child: FlatButton(
+                                    onPressed: () {},
+                                    child: Image.asset(
+                                      AppAssets.comment_icon,
+                                      color: Color.fromRGBO(104, 118, 132, 1),
                                     ),
                                   ),
-
-                                ],
-                              ),
-                              title: Column(
-                                children: [
-                                  Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Icon(
-                                        Icons.favorite,
-                                        size: 12,
-                                        color: Color.fromRGBO(104, 118, 132, 1),
-                                      ),
-                                      Text(
-                                        '  Twitter Home',
-                                        style: TextStyle(fontSize: 10),
-                                      )
-                                    ],
+                                ),
+                                Container(
+                                  width: 50,
+                                  child: FlatButton(
+                                    onPressed: () {},
+                                    child: Image.asset(
+                                      AppAssets.retweet_icon,
+                                      color: Color.fromRGBO(104, 118, 132, 1),
+                                    ),
                                   ),
-                                  SizedBox(height: 5),
-                                  Row(
-                                    children: [
-                                      Text(
-                                        '${snap.data[index].name.toString()}',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Icon(Icons.verified,
-                                          size: 17, color: Colors.blue),
-                                      Text(
-                                        '@',
-                                        style: TextStyle(fontSize: 15),
-                                      ),
-                                      Expanded(
-                                        child: Text(
-                                          '${snap.data[index].username.toString()} - 10h',
-                                          style: TextStyle(fontSize: 14),
-                                        ),
-                                      ),
-                                      Icon(
-                                        Icons.keyboard_arrow_down_rounded,
-                                        size: 18,
-                                        color: Color.fromRGBO(189, 197, 205, 1),
-                                      ),
-                                    ],
+                                ),
+                                Container(
+                                  width: 50,
+                                  child: FlatButton(
+                                    onPressed: () {},
+                                    child: Image.asset(
+                                      AppAssets.heart_icon,
+                                      color: Color.fromRGBO(104, 118, 132, 1),
+                                    ),
                                   ),
-                                  SizedBox(height: 10),
-                                  Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        child: Text(
-                                            'TechPost TechPost TechPost TechPost TechPost TechPost TechPost TechPost TechPost TechPost'),
-                                      ),
-                                    ],
+                                ),
+                                Container(
+                                  width: 50,
+                                  child: FlatButton(
+                                    onPressed: () {},
+                                    child: Image.asset(
+                                      AppAssets.share_icon,
+                                      color: Color.fromRGBO(104, 118, 132, 1),
+                                    ),
                                   ),
-                                  SizedBox(height: 15),
-                                  Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    children: [
-                                      Container(
-                                        width: 50,
-                                        child: FlatButton(
-                                          onPressed: () {},
-                                          child: Image.asset(
-                                            AppAssets.comment_icon,
-                                            color: Color.fromRGBO(
-                                                104, 118, 132, 1),
-                                          ),
-                                        ),
-                                      ),
-                                      Container(
-                                        width: 50,
-                                        child: FlatButton(
-                                          onPressed: () {},
-                                          child: Image.asset(
-                                            AppAssets.retweet_icon,
-                                            color: Color.fromRGBO(
-                                                104, 118, 132, 1),
-                                          ),
-                                        ),
-                                      ),
-                                      Container(
-                                        width: 50,
-                                        child: FlatButton(
-                                          onPressed: () {},
-                                          child: Image.asset(
-                                            AppAssets.heart_icon,
-                                            color: Color.fromRGBO(
-                                                104, 118, 132, 1),
-                                          ),
-                                        ),
-                                      ),
-                                      Container(
-                                        width: 50,
-                                        child: FlatButton(
-                                          onPressed: () {},
-                                          child: Image.asset(
-                                            AppAssets.share_icon,
-                                            color: Color.fromRGBO(
-                                                104, 118, 132, 1),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-                )
-              ],
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
             );
           }
         },
@@ -200,3 +184,7 @@ class User {
 
   User(this.name, this.username);
 }
+//Sir please check code.
+//one error in this code
+//to scroll list and show invalid range...
+//how can i solve
