1fd01bdb0adf450ebe24d043d0eb79a72a40aa0e
Shoeb <shoeb5823@gmail.com>
2021-05-07 16:44:58 +0530

    'user_feature'

--- lib/getAll_projectfile.dart
+++ lib/getAll_projectfile.dart
@@ -5,8 +5,6 @@ export 'package:provider/provider.dart';
 export 'package:tech_post_app/class_model/post_model.dart';
 export 'package:tech_post_app/class_model/postwithusername_model.dart';
 export 'package:tech_post_app/imagefile.dart';
-export 'package:tech_post_app/show_screen/show_api_data.dart';
-
 export "package:flutter/material.dart";
 export 'package:tech_post_app/api_file/api_services.dart';
 
