b63a9a95a0d03a0e19a48643a339000f7d0a634b
Shoeb <shoeb5823@gmail.com>
2021-05-07 16:43:38 +0530

    'user_feature'

--- lib/getAll_projectfile.dart
+++ lib/getAll_projectfile.dart
@@ -6,7 +6,7 @@ export 'package:tech_post_app/class_model/post_model.dart';
 export 'package:tech_post_app/class_model/postwithusername_model.dart';
 export 'package:tech_post_app/imagefile.dart';
 export 'package:tech_post_app/show_screen/show_api_data.dart';
-export 'package:tech_post_app/show_screen/twitter_home.dart';
+
 export "package:flutter/material.dart";
 export 'package:tech_post_app/api_file/api_services.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:tech_post_app/getAll_projectfile.dart';
+import 'package:tech_post_app/show_screen/twitter_home.dart';
 void main() {
   runApp(MyApp());
 }
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TwiterHome(),
+      home: TwitterApp(),
 
     );
   }
